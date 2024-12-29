b245fb56ab78c9c121c5cbb8eee2c13bf53d1294
Ari-Wisc <arinoson1@gmail.com>
2024-12-29 16:46:02 -0600

    added HTMLNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,15 @@
-
 from enum import Enum
 
-# Enum for text types
+
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
-# Class to represent text nodes
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -18,11 +17,11 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        if not isinstance(other, TextNode):
-            return False
-        return (self.text == other.text and
-                self.text_type == other.text_type and
-                self.url == other.url)
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
