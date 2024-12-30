a8ed6116f7d594d328b673771651e9ceaf7767cc
Ari-Wisc <arinoson1@gmail.com>
2024-12-30 14:39:56 -0600

    added some functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
     node=TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
+    print(node.text_node_to_html_node())
+
+    print(node.text_node_to_html_node().to_html())
 
 
 if __name__ == "__main__":
